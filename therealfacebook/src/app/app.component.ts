296719719f82777ff692bcb507ed727be82bd859
Tuszmak <daniel.pinter02@gmail.com>
2025-04-10 20:22:27 +0200

    asd

--- therealfacebook/src/app/app.component.ts
+++ therealfacebook/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ElfelejtettComponent } from './elfelejtett/elfelejtett.component';
 import { JelszoComponent } from './jelszo/jelszo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JelszoComponent],
+  imports: [RouterOutlet, JelszoComponent, ElfelejtettComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ therealfacebook/src/app/elfelejtett/elfelejtett.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-elfelejtett',
+  imports: [],
+  templateUrl: './elfelejtett.component.html',
+  styleUrl: './elfelejtett.component.css'
+})
+export class ElfelejtettComponent {
+
+}
