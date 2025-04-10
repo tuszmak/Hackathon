743fba314ae0dc0f2eb2206b94131b9d46e7c6c2
DominikSzasz <dominik@szaszdominik.hu>
2025-04-10 20:20:10 +0200

    asd

--- therealfacebook/src/app/app.component.ts
+++ therealfacebook/src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { JelszoComponent } from './jelszo/jelszo.component';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, JelszoComponent],
+  imports: [RouterOutlet, JelszoComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  standalone: true, // 🔥 Add this line
+
 })
 export class AppComponent {
   title = 'therealfacebook';
+  forgotpass = false
+
+  btnclick(event: Event) {
+    event.preventDefault(); // 🔥 Stop the default link behavior
+    this.forgotpass = !this.forgotpass; // Toggle the state
+  }
 }
