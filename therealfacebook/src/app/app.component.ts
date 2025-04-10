fa80fc702143ffc12a3105c7d74c5944dc102276
DominikSzasz <dominik@szaszdominik.hu>
2025-04-10 19:57:39 +0200

    nem

--- therealfacebook/src/app/app.component.ts
+++ therealfacebook/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JelszoComponent } from './jelszo/jelszo.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JelszoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
